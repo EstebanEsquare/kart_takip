7e357db5d92421e0bce92a76c96369dba28c2a4d
uner_engineering <mustafauner86@gmail.com>
2026-05-08 07:49:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kart-takip-v3';
+const CACHE_NAME = 'kart-takip-v7';
 const assets = [
   'index.html',
   'manifest.json',
@@ -17,6 +17,17 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
     })
   );
 });
